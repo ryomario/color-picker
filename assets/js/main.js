61b6a5a64634349bbd77b2384886c63d31192cdb
Mario <ardamomo3@gmail.com>
2024-09-10 23:25:29 +0700

    custom-color-picker: Initial Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,35 @@
-import { copyText, hex2rgb, rgbToHex, getCSSColorRGB, getRelativeLuminance } from './tools.js';
+import { copyText } from './tools.js';
 window.copyText = copyText;
 
 document.addEventListener('DOMContentLoaded', function() {
+    const Formula = CustomColorPicker.Formula;
     const initColorHex = '#56baed';
-    const initColorRGB = hex2rgb(initColorHex);
-    const inputBGColor = document.getElementById('background_color');
-    inputBGColor.value = initColorHex;
-
     const onInput = function () {
-        const bg = hex2rgb(inputBGColor.value);
+        const bg = Formula.hex2rgb(targetColorInput.value);
 
-        const y = getRelativeLuminance(bg);
-
-        const is_color_light = y > (255 / 2);
+        const is_color_light = Formula.isLightColor(bg);
 
         let fg = '#ffffff';
         if(is_color_light) {
             fg = '#000000';
         }
-        document.body.style.color = getCSSColorRGB(hex2rgb(fg));
-        document.body.style.backgroundColor = getCSSColorRGB(bg);
+        document.body.style.color = fg;
+        document.body.style.backgroundColor = Formula.rgb2hex(bg);
 
         document.getElementById('text_color_txt').value = fg;
-        document.getElementById('background_color_txt').value = inputBGColor.value;
-        document.getElementById('background_color_rgb').value = getCSSColorRGB(bg);
+        document.getElementById('background_color_txt').value = targetColorInput.value;
+        document.getElementById('background_color_rgb').value = Formula.rgb2hex(bg);
     };
-    inputBGColor.addEventListener('input', onInput);
-
-    onInput();
-
-    // RGB picker
-    const inputsRange = this.querySelectorAll('.rgb-color-picker input[type=range]');
-    const targetRGBInput = this.getElementById('background_color');
-    const changeInput = () => {
-        const colorMap = {r:0,g:0,b:0};
-        inputsRange.forEach(input => {
-            const fieldColor = input.getAttribute('data-color');
-            colorMap[fieldColor] = input.value;
-        });
-        targetRGBInput.value = rgbToHex(colorMap);
+    // Custom Color Picker
+    const targetColorInput = this.getElementById('background_color');
+    const canvas = this.getElementById('color-picker');
+    if(!canvas)return
+    const colorPicker = new CustomColorPicker(canvas, {
+        defaultColor: initColorHex
+    });
+    colorPicker.addEventListener('change-color',function(color) {
+        targetColorInput.value = color;
         onInput();
-    }
-    inputsRange.forEach(input => {
-        input.min = 0;
-        input.max = 255;
-        
-        const fieldColor = input.getAttribute('data-color');
-        input.value = initColorRGB[fieldColor];
-        const valuePreviewEl = this.querySelector('.rgb-color-picker .rgb-color-'+fieldColor+' .value');
-        const previewEl = this.querySelector('.rgb-color-picker .rgb-color-'+fieldColor+' .preview');
-
-        const onInputRange = () => {
-            valuePreviewEl.textContent = (input.value);
-            const thisColorMap = {r:0,g:0,b:0};
-            thisColorMap[fieldColor] = input.value;
-            previewEl.style.backgroundColor = getCSSColorRGB(thisColorMap);
-            changeInput();
-        }
-
-        input.addEventListener('input', onInputRange);
-
-        onInputRange();
-    })
+    });
+    colorPicker.setColor('#56baed');
 })
--- assets/js/tools.js
+++ assets/js/tools.js
@@ -8,25 +8,3 @@ export function copyText(idEl) {
 
     alert("Copied the text: " + textEl.value);
 }
-
-export function hex2rgb(hex) {
-    const r = parseInt(hex.slice(1, 3), 16);
-    const g = parseInt(hex.slice(3, 5), 16);
-    const b = parseInt(hex.slice(5, 7), 16);
-    
-    // return {r, g, b} 
-    return { r, g, b };
-}
-export const getRelativeLuminance = (rgb) =>
-    0.2126 * rgb['r']
-    + 0.7152 * rgb['g']
-    + 0.0722 * rgb['b'];
-export const getCSSColorRGB = (rgb) => `rgb(${rgb['r']},${rgb['g']},${rgb['b']})`;
-function decToHex(c) {
-    var hex = Number(c).toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-  
-export function rgbToHex(rgb) {
-    return "#" + decToHex(rgb.r) + decToHex(rgb.g) + decToHex(rgb.b);
-}
